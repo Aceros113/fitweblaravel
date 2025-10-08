32522b8317bf5d59d60722af9d64b02a906013a2
David Aceros <136133547+Aceros113@users.noreply.github.com>
2025-10-08 12:17:58 -0500

    Fix: Correct configuration string in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Exceptions;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureUserHasRole::class,
         ]);
-    });
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+    })
+    ->create();
